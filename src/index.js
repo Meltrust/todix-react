9572a1a3c76c68411fd62b123e57411cc52fdb8c
Meltrust <original.mtapia@outlook.com>
2021-10-27 02:24:44 -0500

    Add styles and destructuring

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import TodoContainer from "./components/TodoContainer";
+import "./App.css"
+
 ReactDOM.render(
-  <React.StrictMode>
+  <div>
     <TodoContainer />
-  </React.StrictMode>,
+  </div>,
   document.getElementById("root")
 )
 
